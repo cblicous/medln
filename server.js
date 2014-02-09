5f3c2d4108aba2f3787b2e28c62eff8a0f221417
cb <cbelka@gmail.com>
2014-02-09 14:26:44 +0100

    inital version

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require('express');
+//var server = express.createServer();
+// express.createServer()  is deprecated. 
+var server = express(); // better instead
+server.configure(function(){
+  server.use('/', express.static('./public' ));
+
+});
+
+server.listen(3000);
